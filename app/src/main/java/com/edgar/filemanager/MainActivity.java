4b60ad38c2dfd5eacf8a7e580530e8eda2bec47b
edgar <xiexiang89@126.com>
2018-10-22 22:36:58 +0800

    Modify main

--- app/src/main/java/com/edgar/filemanager/MainActivity.java
+++ app/src/main/java/com/edgar/filemanager/MainActivity.java
@@ -1,13 +1,35 @@
 package com.edgar.filemanager;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SlidingPaneLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends FileBaseActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private Toolbar mToolBar;
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolBar = findViewById(R.id.tool_bar);
+        setSupportActionBar(mToolBar);
+        mTabLayout = findViewById(R.id.tab_layout);
+        mTabLayout.setSelectedTabIndicatorHeight(0);
+        mTabLayout.setTabMode(TabLayout.MODE_FIXED);
+        mTabLayout.setTabTextColors(ContextCompat.getColor(this,R.color.tab_normal_color),
+                ContextCompat.getColor(this,R.color.tab_select_color));
+        mTabLayout.addTab(mTabLayout.newTab().setText("分类"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("手机"));
     }
 }
